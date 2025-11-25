65e796efed03075c5bcf23d5215af92b41cacaf7
Jafar Marouf <jafar.k.marouf@gmail.com>
2025-11-26 01:36:59 +0300

    Init: Initialize Flutter project and add dependencies

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:mini_social_feed/social_app.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const SocialApp());
+}
---
+++ lib/social_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SocialApp extends StatelessWidget {
+  const SocialApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(debugShowCheckedModeBanner: false, home: Scaffold());
+  }
+}
